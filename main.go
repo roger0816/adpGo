fd9b63eab9765999132781091f816eac6f386701
roger <korm321@gmail.com>
2023-10-13 06:42:03 +0800

    1013

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 
 	args := os.Args[1:] // [1:] 可以跳過程序名稱
-	fmt.Println("v1.1.0922")
+	fmt.Println("v2.1.1013")
 	fmt.Printf("args : %v\n", args)
 	listTen := "6005"
 	dbIp := "172.104.112.34"
