08041d1b9995867eb2a5021dd9bcdacda63e2f9d
roger <korm321@gmail.com>
2025-06-25 20:53:49 +0000

    change run shell

--- run_adp.sh
+++ run_adp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-pkill -f "./adpGo"
+killall adpGo
 
 ./adpGo 6101 172.104.117.7 adp > /dev/null 2>&1 &
 ./adpGo 6201 172.104.117.7 adp > /dev/null 2>&1 &
--- test_adp.sh
+++ test_adp.sh
@@ -2,8 +2,9 @@
 
 
 
+killall adpGoTest
+
 cp adpGo adpGoTest
 
-pkill -f "./adpGoTest"
 
 ./adpGoTest 6103 172.104.112.34 adp > /dev/null 2>&1 &
