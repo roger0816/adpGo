a97262e72bd4e9099d1cff4aaf305576a457cc9d
roger <korm321@gmail.com>
2025-06-28 08:21:34 +0800

    change run shell

--- scripts/run_api_adp.sh
+++ scripts/run_api_adp.sh
@@ -3,5 +3,5 @@
 
 cd ../bin
 
-./adpApi 6103 172.104.117.7 adp > /dev/null 2>&1 &
+./adpApi 6105 172.104.117.7 adp > /dev/null 2>&1 &
 
--- scripts/run_api_adt.sh
+++ scripts/run_api_adt.sh
@@ -5,6 +5,9 @@
 
 cd ../bin
    
-./adpApi 6103 159.223.54.194 adt1 > /dev/null 2>&1 &
-./adpApi 6103 159.223.54.194 adt2 > /dev/null 2>&1 &
-./adpApi 6103 159.223.54.194 adt3 > /dev/null 2>&1 &
+./adpApi 6105 159.223.54.194 adt1 > /dev/null 2>&1 &
+./adpApi 6205 159.223.54.194 adt2 > /dev/null 2>&1 &
+./adpApi 6305 159.223.54.194 adt3 > /dev/null 2>&1 &
+./adpApi 6405 159.223.54.194 adt4 > /dev/null 2>&1 &
+./adpApi 6505 159.223.54.194 adt5 > /dev/null 2>&1 &
+./adpApi 6605 159.223.54.194 adt6 > /dev/null 2>&1 &
